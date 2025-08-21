4a205a694553e48ff4264862a5be5d26c1e1e432
Rohit Tegginamath <rohittegginamath78@gmail.com>
2025-08-21 17:53:03 +0530

    added cards to the page

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className=" h-screen w-screen">
+    <div className=" h-screen w-screen flex">
       <nav className="w-[15%] h-full bg-zinc-100 flex flex-col items-center pt-5">
         <a href="/" className="px-5 py-3 rounded-xl border-blue-300 border">
           Add new Product
@@ -28,6 +28,138 @@ function App() {
           </li>
         </ul>
       </nav>
+      <div className=" h-full w-[85%] p-10 pt-[5%] flex flex-wrap overflow-x-hidden overflow-y-auto">
+        <div className="card p-5 mr-3 mb-3 border-zinc-400 rounded shadow w-[18%] h-[30vh] flex flex-col justify-center items-center">
+          <div
+            className="hover:scale-110 mb-3  w-full h-[80%] bg-contain bg-no-repeat bg-center"
+            style={{
+              backgroundImage:
+                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png)",
+            }}
+          ></div>
+          <h1>Lorem ipsum dolor sit.</h1>
+        </div>
+        <div className="card p-5 border-zinc-400 rounded shadow w-[18%] h-[30vh] flex flex-col justify-center items-center">
+          <div
+            className="hover:scale-110 mb-3  w-full h-[80%] bg-contain bg-no-repeat bg-center"
+            style={{
+              backgroundImage:
+                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png)",
+            }}
+          ></div>
+          <h1>Lorem ipsum dolor sit.</h1>
+        </div>
+        <div className="card p-5 border-zinc-400 rounded shadow w-[18%] h-[30vh] flex flex-col justify-center items-center">
+          <div
+            className="hover:scale-110 mb-3  w-full h-[80%] bg-contain bg-no-repeat bg-center"
+            style={{
+              backgroundImage:
+                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png)",
+            }}
+          ></div>
+          <h1>Lorem ipsum dolor sit.</h1>
+        </div>
+        <div className="card p-5 border-zinc-400 rounded shadow w-[18%] h-[30vh] flex flex-col justify-center items-center">
+          <div
+            className="hover:scale-110 mb-3  w-full h-[80%] bg-contain bg-no-repeat bg-center"
+            style={{
+              backgroundImage:
+                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png)",
+            }}
+          ></div>
+          <h1>Lorem ipsum dolor sit.</h1>
+        </div>
+        <div className="card p-5 border-zinc-400 rounded shadow w-[18%] h-[30vh] flex flex-col justify-center items-center">
+          <div
+            className="hover:scale-110 mb-3  w-full h-[80%] bg-contain bg-no-repeat bg-center"
+            style={{
+              backgroundImage:
+                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png)",
+            }}
+          ></div>
+          <h1>Lorem ipsum dolor sit.</h1>
+        </div>
+        <div className="card p-5 border-zinc-400 rounded shadow w-[18%] h-[30vh] flex flex-col justify-center items-center">
+          <div
+            className="hover:scale-110 mb-3  w-full h-[80%] bg-contain bg-no-repeat bg-center"
+            style={{
+              backgroundImage:
+                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png)",
+            }}
+          ></div>
+          <h1>Lorem ipsum dolor sit.</h1>
+        </div>
+        <div className="card p-5 border-zinc-400 rounded shadow w-[18%] h-[30vh] flex flex-col justify-center items-center">
+          <div
+            className="hover:scale-110 mb-3  w-full h-[80%] bg-contain bg-no-repeat bg-center"
+            style={{
+              backgroundImage:
+                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png)",
+            }}
+          ></div>
+          <h1>Lorem ipsum dolor sit.</h1>
+        </div>
+        <div className="card p-5 border-zinc-400 rounded shadow w-[18%] h-[30vh] flex flex-col justify-center items-center">
+          <div
+            className="hover:scale-110 mb-3  w-full h-[80%] bg-contain bg-no-repeat bg-center"
+            style={{
+              backgroundImage:
+                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png)",
+            }}
+          ></div>
+          <h1>Lorem ipsum dolor sit.</h1>
+        </div>
+        <div className="card p-5 border-zinc-400 rounded shadow w-[18%] h-[30vh] flex flex-col justify-center items-center">
+          <div
+            className="hover:scale-110 mb-3  w-full h-[80%] bg-contain bg-no-repeat bg-center"
+            style={{
+              backgroundImage:
+                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png)",
+            }}
+          ></div>
+          <h1>Lorem ipsum dolor sit.</h1>
+        </div>
+        <div className="card p-5 border-zinc-400 rounded shadow w-[18%] h-[30vh] flex flex-col justify-center items-center">
+          <div
+            className="hover:scale-110 mb-3  w-full h-[80%] bg-contain bg-no-repeat bg-center"
+            style={{
+              backgroundImage:
+                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png)",
+            }}
+          ></div>
+          <h1>Lorem ipsum dolor sit.</h1>
+        </div>
+        <div className="card p-5 border-zinc-400 rounded shadow w-[18%] h-[30vh] flex flex-col justify-center items-center">
+          <div
+            className="hover:scale-110 mb-3  w-full h-[80%] bg-contain bg-no-repeat bg-center"
+            style={{
+              backgroundImage:
+                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png)",
+            }}
+          ></div>
+          <h1>Lorem ipsum dolor sit.</h1>
+        </div>
+        <div className="card p-5 border-zinc-400 rounded shadow w-[18%] h-[30vh] flex flex-col justify-center items-center">
+          <div
+            className="hover:scale-110 mb-3  w-full h-[80%] bg-contain bg-no-repeat bg-center"
+            style={{
+              backgroundImage:
+                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png)",
+            }}
+          ></div>
+          <h1>Lorem ipsum dolor sit.</h1>
+        </div>
+        <div className="card p-5 border-zinc-400 rounded shadow w-[18%] h-[30vh] flex flex-col justify-center items-center">
+          <div
+            className="hover:scale-110 mb-3  w-full h-[80%] bg-contain bg-no-repeat bg-center"
+            style={{
+              backgroundImage:
+                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png)",
+            }}
+          ></div>
+          <h1>Lorem ipsum dolor sit.</h1>
+        </div>
+      </div>
     </div>
   );
 }
