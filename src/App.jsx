b0761465192d606f02477c963c9141e16410eee7
Rohit Tegginamath <rohittegginamath78@gmail.com>
2025-08-22 23:27:58 +0530

    Added details page

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Home from "./assets/components/Home";
 import { Routes, Route } from "react-router-dom";
+import Details from "./assets/components/Details";
 function App() {
   return (
     <div className=" h-screen w-screen flex">
       
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/details" element={<Details />} />
+
       </Routes>
     </div>
   );
--- src/assets/components/Details.jsx
+++ src/assets/components/Details.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Details = () => {
   return (
-    <div>
-      Details Page
+    <div className="w-[70%] flex h-full justify-between m-auto p-[10%] ">
+      <img
+        className="object-contain h-[80%] w-[40%]"
+        src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png"
+        alt=""
+      />
+      <div className="content bg-red-10@ w-[50%]">
+        <h1 className="text-4xl">
+          Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+        </h1>
+        <h3 className="text-zinc-400 my-5">men's clothing</h3>
+        <h2 className="text-red-200">$ 109.95</h2>
+        <p>description</p>
+        <Link>Edit</Link>
+        <Link>Delete</Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Details
+export default Details;
